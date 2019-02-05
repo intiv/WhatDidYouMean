38b22a45da30734bfd27b177a0de6045ccab12e1
RGaldamez <ricardo.galdamez@gmail.com>
2019-02-04 21:25:33 -0600

    agregando funcionalidad del traceroute

--- src/didyoumean/Functions.java
+++ src/didyoumean/Functions.java
@@ -5,7 +5,9 @@
  */
 package didyoumean;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JFileChooser;
@@ -15,8 +17,8 @@ import javax.swing.JFileChooser;
  * @author ricar
  */
 public class Functions {
+
     private ArrayList<String> palindromos = new ArrayList();
-        
 
     public Functions() {
         //Se inicia el llenado de palindromos
@@ -26,36 +28,149 @@ public class Functions {
         this.palindromos.add("Yo dono rosas, oro no doy");
         this.palindromos.add("Anula la luz azul a la Luna");
     }
-    
-    
-    
+
     //funcion para llamar un palindromo
-    public void ls (){
+    public void ls() {
         Random random = new Random();
         System.out.println(this.palindromos.get(random.nextInt(5)));
-        
+
     }
-    
+
     //genera carita ascii dentro de un folder
-    public void traceroute(){
+    public void traceroute() {
+
+        //Creacion de la cara ASCII
+        String face = "";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 0 && j == 5) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 1 && j == 3) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 3 && j == 2) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 1 && j == 7) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 3 && j == 8) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 3 && j == 4) {
+                    //System.out.print("##");
+                    face += "##";
+                } else if (i == 3 && j == 6) {
+                    //System.out.print("##");
+                    face += "##";
+                } else if (i == 4 && j == 4) {
+                    //System.out.print("##");
+                    face += "##";
+                } else if (i == 4 && j == 6) {
+                    //System.out.print("##");
+                    face += "##";
+                } else if (i == 4 && j == 0) {
+                    //System.out.print(" ");
+
+                    face += " ";
+                }
+                if (i == 3 || i == 4) {
+                    if (j == 5) {
+                        //System.out.print("");
+
+                    } else {
+                        //System.out.print("   ");
+
+                        face += "   ";
+                    }
+                } else {
+                    if (i != 6) {
+                        //System.out.print("   ");
+                        face += "   ";
+                    } else {
+
+                    }
+
+                }
+
+            }
+            face += "\n";
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 3 && j == 5) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 2 && j == 3) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 1 && j == 2) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 2 && j == 7) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 1 && j == 8) {
+                    //System.out.print("*");
+                    face += "*";
+                } else if (i == 1 && j == 4) {
+                    //System.out.print("______/");
+
+                    face += "______/";
+                }
+
+                if (i == 1 && j == 4 || i == 1 && j == 5) {
+
+                } else {
+                    //System.out.print("   ");
+                    face += "   ";
+                }
+
+            }
+            if (i == 1) {
+                //System.out.println("");
+                face += "\n";
+            }
+            //System.out.println("");
+            face += "\n";
+
+        }
+        //System.out.println(face);
+
         
         //Creacion de la carpeta con el nombre que le da el usuario
         JFileChooser fileChooser = new JFileChooser();
         int status = fileChooser.showSaveDialog(fileChooser);
-        
-        if (status == JFileChooser.APPROVE_OPTION){
+
+        if (status == JFileChooser.APPROVE_OPTION) {
             File directory = fileChooser.getSelectedFile();
             boolean created = directory.mkdir();
-            if(created){
+            if (created) {
                 //System.out.println("Archivo creado correctamente");
-            }else{
+            } else {
                 System.err.println("Error con la creacion del archivo");
             }
-                    
+            String path = directory.getAbsolutePath();
+
+            //System.out.println(path);
+
+            File smileyfile = new File(path + "\\smiley.txt");
+            try {
+                FileWriter fw = new FileWriter(smileyfile);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(face);
+                bw.flush();
+                
+                bw.close();
+                fw.close();
+            } catch (Exception e) {
+                
+            }
+
         }
-        
-        
-        
+
     }
-    
+
 }
